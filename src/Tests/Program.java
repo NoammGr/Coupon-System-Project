d12a0ae9f907a4f297ccabd85943c943644ebc02
Noam Greedi <noamgreedi45@gmail.com>
2022-11-19 14:50:39 +0200

    .

--- src/Tests/Program.java
+++ src/Tests/Program.java
@@ -5,7 +5,7 @@ package Tests;
  * in order to activate
  * this class is go to Test class and remove '//' from the line you want to
  * check , also I made
- * custome object if you want check any insert to data base all you need to do remove is "//"
+ * custome object if you want check any insert to data base all you need to do is remove "//"
  */
 public class Program {
     public static void main(String[] args) {
